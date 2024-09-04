f09274c8cea1d6dfb7ad6a71e93400a0e42b78c2
Felipe Moreira <felipemp2012@hotmail.com>
2024-09-04 14:35:06 -0300

    atualizacao

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from dtos.novo_produto_dto import NovoProdutoDTO
+from dtos.inserir_produto_dto import InserirProdutoDTO
+from models.produto_model import Produto
 from repositories.produto_repo import ProdutoRepo
 
 
@@ -12,6 +13,8 @@ async def obter_produtos():
     produtos = ProdutoRepo.obter_todos()
     return produtos
 
-@router.get("/inseir_produto")
-async def inseir_produto(produtos: NovoProdutoDTO):
-    return produtos
+@router.post("/inserir_produto")
+async def inserir_produto(produto: InserirProdutoDTO) -> Produto:
+    novo_produto = Produto(None, produto.nome, produto.preco, produto.descricao, produto.estoque)
+    novo_produto = ProdutoRepo.inserir(novo_produto)
+    return novo_produto
