2bed95a3cfc883b866a878a34bef9c7772be2490
Felipe Moreira <felipemp2012@hotmail.com>
2024-09-18 13:34:18 -0300

    att

--- dtos/usuario_autenticado.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-
-@dataclass
-class UsuarioAutenticado:    
-    nome: Optional[str] = None
-    email: Optional[str] = None
-    perfil: Optional[int] = None
